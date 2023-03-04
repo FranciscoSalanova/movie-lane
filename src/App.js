bbe5d5cda69bd8527cfd0889ecce13cf1f692b89
FranciscoSalanova <fsalanova.16@gmail.com>
2023-03-04 13:07:43 -0300

    installing dependencies localy, editing styles and adding search bar and movie slot

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { useEffect } from "react"
 import './App.css'
-import './search.svg'
+import SearchIcon from './search.svg'
 
 // d92b318 -> OMDB API key
 
@@ -15,13 +15,48 @@ const App = () => {
         console.log(data.Search)
     }
 
+    const movie1 = {
+        "Title": "Amazing Spiderman Syndrome",
+        "Year": "2012",
+        "imdbID": "tt2586634",
+        "Type": "movie",
+        "Poster": "N/A"
+    }
+
     useEffect(() => {
         searchMovies('Spiderman')
     }, [])
 
     return (
         <div className="app">
-            <h1>App</h1>
+            <h1>MovieLand</h1>
+            <div className="search">
+                <input 
+                    placeholder="Search for movies"
+                    value="Superman"
+                    onChange={() => {}}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => {}}
+                />
+            </div>
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>
+                            {movie1.Year}
+                        </p>
+                    </div>
+                    <div>
+                        <img
+                            src={movie1.Poster !== "N/A" ? movie1.Poster : 'https://via.placeholder.com/400'}
+                            alt={movie1.Title}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
