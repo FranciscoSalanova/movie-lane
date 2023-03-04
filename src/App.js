c92a8f5c1ae54aa0c8debd5ac6864f35dc55c3fe
FranciscoSalanova <fsalanova.16@gmail.com>
2023-03-04 18:10:53 -0300

    finishing with styles, generating Movie component and adding searching functionality

--- src/App.js
+++ src/App.js
@@ -1,62 +1,59 @@
 import React from "react"
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
 import './App.css'
 import SearchIcon from './search.svg'
+import Movie from './components/Movie'
 
 // d92b318 -> OMDB API key
 
 const API_URL = 'http://www.omdbapi.com?apikey=d92b318'
 
 const App = () => {
+    const [movies, setMovies] = useState()
+    const [searchTerm, setSearchTerm] = useState('')
+
     const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s={title}`)
+        const response = await fetch(`${API_URL}&s=${title}`)
         const data = await response.json()
 
-        console.log(data.Search)
+        setMovies(data.Search)
     }
 
-    const movie1 = {
-        "Title": "Amazing Spiderman Syndrome",
-        "Year": "2012",
-        "imdbID": "tt2586634",
-        "Type": "movie",
-        "Poster": "N/A"
-    }
-
-    useEffect(() => {
-        searchMovies('Spiderman')
-    }, [])
+    // useEffect(() => {
+    //     searchMovies('Spiderman')
+    // }, [])
 
     return (
         <div className="app">
-            <h1>MovieLand</h1>
+            <h1>MovieLane</h1>
             <div className="search">
                 <input 
                     placeholder="Search for movies"
-                    value="Superman"
-                    onChange={() => {}}
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <img
                     src={SearchIcon}
                     alt="search"
-                    onClick={() => {}}
+                    onClick={() => searchMovies(searchTerm)}
                 />
             </div>
-            <div className="container">
-                <div className="movie">
-                    <div>
-                        <p>
-                            {movie1.Year}
-                        </p>
-                    </div>
-                    <div>
-                        <img
-                            src={movie1.Poster !== "N/A" ? movie1.Poster : 'https://via.placeholder.com/400'}
-                            alt={movie1.Title}
-                        />
-                    </div>
-                </div>
-            </div>
+            {
+                movies?.length > 0
+                    ? (
+                        <div className="container">
+                            {
+                                movies.map((movie) => (
+                                    <Movie movie={movie} />
+                                ))
+                            }
+                        </div>
+                    ) : (
+                        <div className="empty">
+                            <h2>No movies found</h2>
+                        </div>
+                    )
+            }
         </div>
     )
 }
